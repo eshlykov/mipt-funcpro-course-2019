d29707735682232d586c84bbe1452b96a9ecfb51
Evgeny Shlykov <e.a.shlykov@gmail.com>
2019-04-15 14:05:20 +0000

    add fourth lab

---
+++ labs/lab-4.fs
@@ -0,0 +1,34 @@
+// Во всех заданиях запрещено использовать цикл for, переменные (mutable) и
+// ссылочные значения (указатели, ref).
+
+// Задание 1 (1 балл). Без использования функций из модуля Seq получить
+// последовательность 1, 2, 3, 4, 5, 6, ... (натуральные числа).
+let naturals = failwith "Not implemented"
+
+// Задание 2 (1 балл). Без использования функций из модуля Seq получить
+// последовательность 0, 1, 0, 2, 0, 3, ... (нечетные элементы - нули, четные -
+// последовательность натуральных чисел).
+let oddNaturals = failwith "Not implemented"
+
+// Задание 3 (1 балл). Без использования модуля Seq реализовать функцию
+// Seq.initInfinite.
+let seqInitInfinite initializer = failwith "Not implemented"
+
+// Задание 4 (1 балл). Без использования модуля Seq реализовать функцию
+// Seq.unfold.
+let seqUnfold generator state = failwith "Not implemented"
+
+// Задание 5 (1 балл). Протестируйте вашу функцию seqUnfold, получив с ее
+// помощью последовательность 0, 1, 2, 2, 3, 5, ... (числа Фибоначчи).
+let fibonaccies = failwith "Not implemented"
+
+// Задание 6 (1 балл). Реализовать функцию Seq.chunkBySize без использования
+// функции Seq.chunkBySize.
+let rec seqChunkBySize size sequence = failwith "Not implemented"
+
+// Задание 7 (1 балл). Реализовать функцию Seq.pairwise без использования
+// функции Seq.pairwise.
+let rec seqPairwise sequence = failwith "Not implemented"
+
+// Задание 8 (1 балл). Выберите произвольную функцию из модуля Seq и реализуйте
+// ее без использования модуля Seq.
